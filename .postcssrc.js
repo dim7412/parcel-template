d314c5709e191f9d88976500c88a9089d155d53b
ohdonglim <dim7412@nate.com>
2021-09-18 16:28:17 +0900

    Creat project

---
+++ .postcssrc.js
@@ -0,0 +1,22 @@
+// ESM
+// CommonJS
+
+// import autoprefixer from 'autoprefixer'
+//const autoprefixer = require('autoprefixer')
+
+//export {
+//  plugins: [
+//    autoprefixer
+//  ]
+//}
+//module.exports ={
+//  Plugin: [
+//    autoprefixer
+//  ]
+//}
+
+module.exports ={
+  Plugin: [
+    require('autoprefixer')
+  ]
+}
